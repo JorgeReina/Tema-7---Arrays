657c25813b932030a05a00c2862c69004a7465b4
Jorge Reina Romero <reinaromerojorge@gmail.com>
2023-01-16 19:44:06 +0100

    Ficheros.java

--- Ejercicio18.java
+++ Ejercicio18.java
@@ -21,5 +21,63 @@ public class Ejercicio18 {
 		
 		Scanner datos = new Scanner(System.in);
 		
+		int [] n = new int [10];
+		int [] mayor = new int[10];
+		int [] menor = new int[10];
+		
+		int	contador = 0;
+		int contador1 = 0;
+		int contador2 = 0;
+		int contador3 = 0;
+		
+		System.out.println("Array original:");
+		System.out.println();
+		
+		System.out.println("Indice	0	 1	 2	 3	 4	 5	 6	 7	 8	 9");
+		System.out.print("Valor ");
+		
+		for (int i = 0; i < 10; i++) {
+			n[i] = (int)(Math.random()*201);
+			System.out.print(" " + n[i] + "	");
+		}
+		
+		System.out.println();
+		System.out.println("Array resultado:");
+		System.out.println();
+		
+		for (int i = 0; i < 10; i++) {
+			if (n[i] > 100) {
+				mayor[contador] = n[i];
+				contador += 1;
+			}
+			if (n[i] < 100) {
+				menor[contador1] = n[i];
+				contador1 += 1;
+			}
+		}
+		
+		for (int i = 0; i < 10; i++)
+		{
+			System.out.print(mayor[i] + " ");
+		}
+		
+		System.out.println();
+		
+		for (int i = 0; i < 10; i++)
+		{
+			System.out.print(menor[i] + " ");
+		}
+		
+		
+		for (int i = 0; i < 10; i+2) {
+				n[i] = minimo[contador2];
+				contador2 += 1;
+		}
+		for (int i = 1; i < 10; i+2) {
+				n[i] = maximo[contador3];
+				contador3 += 1;
+		}
+		
+		
 	}
 }
