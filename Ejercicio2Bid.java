4ccf6b9a554ac06e7f3ffb3f0c549e79f4b9900e
JorgeReina <reinaromerojorge@gmail.com>
2023-01-20 17:39:08 +0100

    Fichero.java

--- Ejercicio2Bid.java
+++ Ejercicio2Bid.java
@@ -31,9 +31,24 @@ public class Ejercicio2Bid {
 			System.out.println();
 		}
 		
-		for (fila = 0; fila < 4; fila++) {
+		for (fila = 4; fila < 5; fila++) {
+			for (columna = 5; columna < 6; columna++) {
+				for (int i = 0; i < 4; i++)
+				{
+					for (int u = 0; u < 5; u++)
+					{
+						n[fila][columna] += n[i][u];
+					}
+					
+				}
+				
+			}
+			System.out.println();
+		}
+		
+		for (fila = 0; fila < 5; fila++) {
 			
-			for (columna = 0; columna < 5; columna++) {
+			for (columna = 0; columna < 6; columna++) {
 				System.out.printf("%5s", n[fila][columna]);
 			}
 			System.out.println();
